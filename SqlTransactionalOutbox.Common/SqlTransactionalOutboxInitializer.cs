6e84cdb91dfa802524e97f4660629f48133d0c9d
Brandon Bernard <brandon.bernard@exlrt.com>
2022-08-19 14:25:59 -0500

    Changed configuration to be more fluent with ConfigBuilder for better encapsulation (better consistency with AspNetCore initialization).

--- SqlTransactionalOutbox.Common/SqlTransactionalOutboxInitializer.cs
+++ SqlTransactionalOutbox.Common/SqlTransactionalOutboxInitializer.cs
@@ -1,44 +1,72 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using SqlTransactionalOutbox.CustomExtensions;
 
 namespace SqlTransactionalOutbox
 {
     public static class SqlTransactionalOutboxDefaults
     {
-        public static OutboxTableConfig OutboxTableConfig { get; internal set; } = new OutboxTableConfig();
+        public static ISqlTransactionalOutboxTableConfig OutboxTableConfig { get; internal set; } = new OutboxTableConfig();
         public static int DistributedMutexAcquisitionTimeoutSeconds { get; internal set; } = 1;
         public static string DistributeMutexLockPrefix { get; internal set; } = "SqlServerTransactionalOutboxProcessor::";
     }
 
     public class SqlTransactionalOutboxInitializer
     {
-        /// <summary>
-        /// Initialize the global default settings for the OutboxTableConfig which will be supported by all convenience methods (e.g. Sql Custom Extensions)!
-        /// NOTE: This should ONLY be called once at application startup and any thread concerns must be manually controlled by the calling code!
-        /// </summary>
-        /// <param name="customConfig"></param>
-        public static void SetOutboxTableConfig(OutboxTableConfig customConfig)
+        private static readonly object _padLock = new object();
+
+        public static SqlTransactionalOutboxInitializer Configure(Action<ConfigBuilder> configAction)
         {
-            SqlTransactionalOutboxDefaults.OutboxTableConfig = customConfig.AssertNotNull(nameof(customConfig));
+            lock (_padLock)
+            {
+                configAction?.Invoke(new ConfigBuilder());
+            }
+
+            // Does nothing currently but may in the future...
+            return new SqlTransactionalOutboxInitializer();
         }
 
-        /// <summary>
-        /// Initialize the global default settings for the Distributed Mutex settings which will be supported by all convenience methods (e.g. Sql Custom Extensions)!
-        /// NOTE: This should ONLY be called once at application startup and any thread concerns must be manually controlled by the calling code!
-        /// </summary>
-        /// <param name="distributedMutexAcquisitionTimeoutSeconds"></param>
-        /// <param name="distributedMutexLockPrefix"></param>
-        public void SetDistributedMutexLockConfig(
-            int? distributedMutexAcquisitionTimeoutSeconds = null,
-            string distributedMutexLockPrefix = null
-        )
+        public class ConfigBuilder
         {
-            if(distributedMutexAcquisitionTimeoutSeconds >= 0)
-                SqlTransactionalOutboxDefaults.DistributedMutexAcquisitionTimeoutSeconds = (int)distributedMutexAcquisitionTimeoutSeconds;
+            /// <summary>
+            /// Prevent external construction to enforce our Configuration Syntax and protect future enhancements with less risk of breaking changes...
+            /// </summary>
+            internal ConfigBuilder()
+            { }
 
-            if (!string.IsNullOrWhiteSpace(distributedMutexLockPrefix))
-                SqlTransactionalOutboxDefaults.DistributeMutexLockPrefix = distributedMutexLockPrefix;
+            /// <summary>
+            /// Initialize the global default settings for the OutboxTableConfig which will be supported by all convenience methods (e.g. Sql Custom Extensions)!
+            /// NOTE: This should ONLY be called once at application startup and any thread concerns must be manually controlled by the calling code!
+            /// </summary>
+            /// <param name="customConfig"></param>
+            public ConfigBuilder WithOutboxTableConfig(ISqlTransactionalOutboxTableConfig customConfig)
+            {
+                SqlTransactionalOutboxDefaults.OutboxTableConfig = customConfig.AssertNotNull(nameof(customConfig));
+                return this;
+            }
+
+            /// <summary>
+            /// Initialize the global default settings for the Distributed Mutex settings which will be supported by all convenience methods (e.g. Sql Custom Extensions)!
+            /// NOTE: This should ONLY be called once at application startup and any thread concerns must be manually controlled by the calling code!
+            /// </summary>
+            /// <param name="lockAcquisitionTimeoutSeconds"></param>
+            /// <param name="lockNamePrefix"></param>
+            public ConfigBuilder WithDistributedMutexLockSettings(
+                int? lockAcquisitionTimeoutSeconds = null,
+                string lockNamePrefix = null
+            )
+            {
+                if (lockAcquisitionTimeoutSeconds < 0)
+                    throw new ArgumentOutOfRangeException(nameof(lockAcquisitionTimeoutSeconds), "Lock acquisition timeout must be 0 or greater.");
+                else if (lockAcquisitionTimeoutSeconds.HasValue)
+                    SqlTransactionalOutboxDefaults.DistributedMutexAcquisitionTimeoutSeconds = (int)lockAcquisitionTimeoutSeconds;
+
+                //Though NOT Advised, for flexibility we Allow the client to set the Prefix to anything (event empty string) if they choose...
+                if (lockNamePrefix != null)
+                    SqlTransactionalOutboxDefaults.DistributeMutexLockPrefix = lockNamePrefix;
+
+                return this;
+            }
         }
-
     }
 }
--- SqlTransactionalOutbox.IntegrationTests/ConfigurationTests/OutboxTableConfigTests.cs
+++ SqlTransactionalOutbox.IntegrationTests/ConfigurationTests/OutboxTableConfigTests.cs
@@ -56,29 +56,76 @@ namespace SqlTransactionalOutbox.IntegrationTests
         }
 
         [TestMethod]
-        public void TestBuiltInOutboxTableConfigOverridesByInitialization()
+        public void TestSqlTransactionalOutboxInitializationConfigurationBuilder()
         {
             //GET DEFAULTS manually since we will override the Initialization defaults next...
             var defaultTableConfig = new OutboxTableConfig();
             
+            //Get initial Defaults for Distributed Mutex values...
+            var defaultDistributedMutexLockTimeout = SqlTransactionalOutboxDefaults.DistributedMutexAcquisitionTimeoutSeconds;
+            var defaultDistributedMutexPrefix = SqlTransactionalOutboxDefaults.DistributeMutexLockPrefix;
+
             //Now use the global initializer to change and read the custom value setting...
-            SqlTransactionalOutboxInitializer.SetOutboxTableConfig(new OutboxTableConfig(transactionalOutboxSchemaName: CustomSchemaName));
+            SqlTransactionalOutboxInitializer.Configure(config =>
+            {
+                config.WithOutboxTableConfig(new OutboxTableConfig(
+                        transactionalOutboxSchemaName: CustomSchemaName,
+                        transactionalOutboxTableName: "OutboxTable",
+                        pkeyFieldName: "PKeyField",
+                        payloadFieldName: "PayloadField",
+                        uniqueIdentifierFieldName: "UniqueIDField",
+                        fifoGroupingIdentifier: "FifoField",
+                        statusFieldName: "StatusField",
+                        publishTargetFieldName: "PublishTargetField",
+                        publishAttemptsFieldName: "PublishAttemptsField",
+                        createdDateTimeUtcFieldName: "CreatedField"
+                    ))
+                    .WithDistributedMutexLockSettings(
+                        lockAcquisitionTimeoutSeconds: 8,
+                        lockNamePrefix: "OutboxDistributedLock::"
+                    );
+            });
+
             var customSchemaTableConfig = SqlTransactionalOutboxDefaults.OutboxTableConfig;
 
-            //Validate Class references
+            //OutboxTableConfig settings...
             Assert.AreEqual(CustomSchemaName, customSchemaTableConfig.TransactionalOutboxSchemaName);
-            Assert.AreEqual(OutboxTableConfig.DefaultTransactionalOutboxSchemaName, defaultTableConfig.TransactionalOutboxSchemaName);
+            Assert.AreEqual("OutboxTable", customSchemaTableConfig.TransactionalOutboxTableName);
+            Assert.AreEqual("PKeyField", customSchemaTableConfig.PKeyFieldName);
+            Assert.AreEqual("PayloadField", customSchemaTableConfig.PayloadFieldName);
+            Assert.AreEqual("UniqueIDField", customSchemaTableConfig.UniqueIdentifierFieldName);
+            Assert.AreEqual("FifoField", customSchemaTableConfig.FifoGroupingIdentifier);
+            Assert.AreEqual("StatusField", customSchemaTableConfig.StatusFieldName);
+            Assert.AreEqual("PublishTargetField", customSchemaTableConfig.PublishTargetFieldName);
+            Assert.AreEqual("PublishAttemptsField", customSchemaTableConfig.PublishAttemptsFieldName);
+            Assert.AreEqual("CreatedField", customSchemaTableConfig.CreatedDateTimeUtcFieldName);
+
             Assert.AreNotEqual(defaultTableConfig.TransactionalOutboxSchemaName, customSchemaTableConfig.TransactionalOutboxSchemaName);
-            Assert.AreEqual(defaultTableConfig.TransactionalOutboxTableName, customSchemaTableConfig.TransactionalOutboxTableName);
+            Assert.AreNotEqual(defaultTableConfig.TransactionalOutboxTableName, customSchemaTableConfig.TransactionalOutboxTableName);
+            Assert.AreNotEqual(defaultTableConfig.PKeyFieldName, customSchemaTableConfig.PKeyFieldName);
+            Assert.AreNotEqual(defaultTableConfig.PayloadFieldName, customSchemaTableConfig.PayloadFieldName);
+            Assert.AreNotEqual(defaultTableConfig.UniqueIdentifierFieldName, customSchemaTableConfig.UniqueIdentifierFieldName);
+            Assert.AreNotEqual(defaultTableConfig.FifoGroupingIdentifier, customSchemaTableConfig.FifoGroupingIdentifier);
+            Assert.AreNotEqual(defaultTableConfig.StatusFieldName, customSchemaTableConfig.StatusFieldName);
+            Assert.AreNotEqual(defaultTableConfig.PublishTargetFieldName, customSchemaTableConfig.PublishTargetFieldName);
+            Assert.AreNotEqual(defaultTableConfig.PublishAttemptsFieldName, customSchemaTableConfig.PublishAttemptsFieldName);
+            Assert.AreNotEqual(defaultTableConfig.CreatedDateTimeUtcFieldName, customSchemaTableConfig.CreatedDateTimeUtcFieldName);
+
+            Assert.AreEqual(OutboxTableConfig.DefaultTransactionalOutboxSchemaName, defaultTableConfig.TransactionalOutboxSchemaName);
+
+            //Distributed Mutex Lock settings...
+            Assert.AreEqual(8, SqlTransactionalOutboxDefaults.DistributedMutexAcquisitionTimeoutSeconds);
+            Assert.AreEqual("OutboxDistributedLock::", SqlTransactionalOutboxDefaults.DistributeMutexLockPrefix);
+            Assert.AreNotEqual(defaultDistributedMutexLockTimeout, SqlTransactionalOutboxDefaults.DistributedMutexAcquisitionTimeoutSeconds);
+            Assert.AreNotEqual(defaultDistributedMutexPrefix, SqlTransactionalOutboxDefaults.DistributeMutexLockPrefix);
+
 
             //Validate Interface Cast references
-
             var defaultTableConfigAsInterface = (ISqlTransactionalOutboxTableConfig)defaultTableConfig;
             var customOutboxTableConfigAsInterface = (ISqlTransactionalOutboxTableConfig)customSchemaTableConfig;
             Assert.AreEqual(CustomSchemaName, customOutboxTableConfigAsInterface.TransactionalOutboxSchemaName);
             Assert.AreEqual(OutboxTableConfig.DefaultTransactionalOutboxSchemaName, defaultTableConfigAsInterface.TransactionalOutboxSchemaName);
             Assert.AreNotEqual(defaultTableConfigAsInterface.TransactionalOutboxSchemaName, customOutboxTableConfigAsInterface.TransactionalOutboxSchemaName);
-            Assert.AreEqual(defaultTableConfigAsInterface.TransactionalOutboxTableName, customOutboxTableConfigAsInterface.TransactionalOutboxTableName);
         }
     }
 }
