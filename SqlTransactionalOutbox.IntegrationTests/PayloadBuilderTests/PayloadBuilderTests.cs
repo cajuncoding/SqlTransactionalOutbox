aa976da1f681a51bbc0776b21c7d5464d6f94015
Brandon Bernard <brandon.bernard@exlrt.com>
2024-02-29 16:07:49 -0600

    Add Unit test to illustrate the use of PayloadBuilder with complex body payload...

--- SqlTransactionalOutbox.IntegrationTests/PayloadBuilderTests/PayloadBuilderTests.cs
+++ SqlTransactionalOutbox.IntegrationTests/PayloadBuilderTests/PayloadBuilderTests.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Linq;
+using Azure.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SqlTransactionalOutbox.JsonExtensions;
 using SqlTransactionalOutbox.Tests;
 using SqlTransactionalOutbox.Utilities;
@@ -71,6 +76,40 @@ namespace SqlTransactionalOutbox.IntegrationTests
             Assert.AreEqual("HttpProxy-IntegrationTest", payloadBuilder.FifoGroupingId);
             Assert.AreEqual("CajunCoding", payloadBuilder.To);
             Assert.AreEqual("Testing Json Payload from HttpProxy", payloadBuilder.Body);
+        }
+
+        private record ComplexBody(string Message, int[] Ids, Guid[] Guids, bool IsThisABoolean);
+
+        [TestMethod]
+        public void TestPayloadBuilderFromObjectWithComplexBody()
+        {
+            var tempObject = new
+            {
+                Topic = TestConfiguration.AzureServiceBusTopic,
+                FifoGroupingId = "HttpProxy-IntegrationTest",
+                To = "CajunCoding",
+                Body = new ComplexBody(
+                    Message: "This is a complex Body Payload...",
+                    Ids: new[] { 1, 2, 3 },
+                    Guids: new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() },
+                    IsThisABoolean: true
+                )
+            };
+
+
+            var payloadBuilder = PayloadBuilder.FromObject(tempObject);
+
+            Assert.AreEqual(TestConfiguration.AzureServiceBusTopic, payloadBuilder.PublishTarget);
+            Assert.AreEqual("HttpProxy-IntegrationTest", payloadBuilder.FifoGroupingId);
+            Assert.AreEqual("CajunCoding", payloadBuilder.To);
+
+            //Compare and Validate the Complex Body values...
+            var originalBody = tempObject.Body;
+            var payloadBody = JsonConvert.DeserializeObject<ComplexBody>(payloadBuilder.Body);
+            Assert.AreEqual(originalBody.Message, payloadBody.Message);
+            Assert.IsTrue(originalBody.Ids.SequenceEqual(payloadBody.Ids));
+            Assert.IsTrue(originalBody.Guids.SequenceEqual(payloadBody.Guids));
+            Assert.AreEqual(originalBody.IsThisABoolean, payloadBody.IsThisABoolean);
         }
     }
 }
